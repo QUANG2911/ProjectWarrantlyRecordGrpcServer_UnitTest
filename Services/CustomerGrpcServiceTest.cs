fdab5e0f7351d90d71437ed52fcfd89f6cd0c3b3
Quang <minhquang5671@gmail.com>
2025-01-07 09:07:18 +0700

    first commit

---
+++ Services/CustomerGrpcServiceTest.cs
@@ -0,0 +1,113 @@
+﻿
+using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using ProjectWarrantlyRecordGrpcServer.DTO;
+using ProjectWarrantlyRecordGrpcServer.Interface;
+using ProjectWarrantlyRecordGrpcServer.Model;
+using ProjectWarrantlyRecordGrpcServer.Protos;
+using ProjectWarrantlyRecordGrpcServer.Services.Grpc;
+
+
+namespace ProjectWarrantyRecordGrpcServer.Tests.Services
+{
+    public class CustomerGrpcServiceTest
+    {
+        private readonly ICustomerService _mockCustomerService;
+        private readonly ILogger<CustomerGrpcService> _mockLogger;
+        private readonly CustomerGrpcService _customerGrpcService;
+
+        public CustomerGrpcServiceTest()
+        {
+            // Tạo mock cho ICustomerService và ILogger
+            _mockCustomerService = A.Fake<ICustomerService>();
+            _mockLogger = A.Fake<ILogger<CustomerGrpcService>>();
+
+            // Truyền mock vào lớp CustomerGrpcService
+            _customerGrpcService = new CustomerGrpcService(_mockCustomerService, _mockLogger);
+        }
+
+        [Fact]
+        public async Task ListCustomerManagement_ReturnOk()
+        {
+            // Arrange
+            var expectedCustomers = new List<Customer>
+            {
+                new Customer
+                {
+                    IdCustomer = 1,
+                    CustomerAddress = "Tp.Thủ Đức, Tp.Hồ Chí Minh",
+                    CustomerEmail = "anv@gmail.com",
+                    CustomerName = "Nguyễn Văn A",
+                    CustomerPhone = "098xx890xx"
+                }
+            };
+
+            // Cấu hình mock cho ICustomerService
+            A.CallTo(() => _mockCustomerService.GetListCustomer()).Returns(expectedCustomers);
+
+            var request = new GetListCustomerManagementRequest();
+
+            // Act
+            var response = await _customerGrpcService.ListCustomerManagement(request, null);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Single(response.ToCustomerList);
+
+            var firstCustomer = response.ToCustomerList.First();
+            Assert.Equal(1, firstCustomer.IdCusomer);
+            Assert.Equal("Nguyễn Văn A", firstCustomer.CustomerName);
+            Assert.Equal("098xx890xx", firstCustomer.CustomerPhone);
+            Assert.Equal("anv@gmail.com", firstCustomer.CustomerEmail);
+            Assert.Equal("Tp.Thủ Đức, Tp.Hồ Chí Minh", firstCustomer.CustomerAdrress);
+
+            // Kiểm tra tương tác
+            A.CallTo(() => _mockCustomerService.GetListCustomer()).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task ReadCustomerManagement_ReturnOk()
+        {
+            // Arrange
+            var expectedCustomers = new List<DetailCustomerDto>
+            {
+                new DetailCustomerDto
+                {
+                    IdCusomer = 1,
+                    CustomerAdrress = "Tp.Thủ Đức, Tp.Hồ Chí Minh",
+                    CustomerEmail = "anv@gmail.com",
+                    CustomerName = "Nguyễn Văn A",
+                    CustomerPhone = "098xx890xx",
+                    CustomerDevice = "Máy tính",
+                    DateOfWarrant = DateOnly.Parse("12/01/2024"),
+                    IdWarrantReport = 1,
+                    TimeEnd = DateOnly.Parse("12/01/2025")
+                }
+            };
+
+            // Cấu hình mock cho ICustomerService
+            var request = new ReadCustomerRequest { IdCusomer = 1 };
+            A.CallTo(() => _mockCustomerService.GetDetailCustomer(request.IdCusomer)).Returns(expectedCustomers);
+            
+
+            //Act
+            var response = await _customerGrpcService.ReadCustomerManagement(request, null);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Single(response.ToDeviceList);
+
+            var firstCustomer = response.ToDeviceList.First();
+            Assert.Equal(1, firstCustomer.IdCusomer);
+            Assert.Equal("Nguyễn Văn A", firstCustomer.CustomerName);
+            Assert.Equal("098xx890xx", firstCustomer.CustomerPhone);
+            Assert.Equal("anv@gmail.com", firstCustomer.CustomerEmail);
+            Assert.Equal("Tp.Thủ Đức, Tp.Hồ Chí Minh", firstCustomer.CustomerAdrress);
+            Assert.Equal("Máy tính", firstCustomer.CustomerDevice);
+            Assert.Equal("12/1/2024", firstCustomer.DateOfWarrant);
+            Assert.Equal("12/1/2025", firstCustomer.TimeEnd);
+            // Kiểm tra tương tác
+            A.CallTo(() => _mockCustomerService.GetDetailCustomer(request.IdCusomer)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ Services/StaffTaskServiceTest.cs
@@ -0,0 +1,153 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using ProjectWarrantlyRecordGrpcServer.DTO;
+using ProjectWarrantlyRecordGrpcServer.Interface;
+using ProjectWarrantlyRecordGrpcServer.Protos;
+using ProjectWarrantlyRecordGrpcServer.Services.Grpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWarrantyRecordGrpcServer.Tests.Services
+{
+    public class StaffTaskServiceTest
+    {
+        private readonly IStaffTaskService _mockTaskService;
+        private readonly ILogger<StaffTaskGrpcService> _mockLogger;
+        private readonly StaffTaskGrpcService _taskGrpcService;
+
+        public StaffTaskServiceTest()
+        {
+            // Tạo mock cho ICustomerService và ILogger
+            _mockTaskService = A.Fake<IStaffTaskService>();
+            _mockLogger = A.Fake<ILogger<StaffTaskGrpcService>>();
+
+            // Truyền mock vào lớp CustomerGrpcService
+            _taskGrpcService = new StaffTaskGrpcService(_mockTaskService, _mockLogger);
+        }
+
+        [Fact]
+        public async Task CreateRepairManagement_ReturnOk()
+        {
+            //Arrange
+            var request = new CreateRepairManagementRequest
+            {
+                ReasonBringFix = "Bị hỏng màn",
+                CustomerEmail = "anv@gmail.com",
+                CustomerName = "Nguyễn Văn A",
+                CustomerPhone = "098xx890xx",
+                DeviceName = "Máy Tính",
+                IdWarrantRecord = 1
+            };
+
+            int expectedTask = 1; // id task sửa chữa mới
+
+            // Cấu hình mock cho ICustomerService
+            A.CallTo(() => _mockTaskService.CreateNewStaffTask(request)).Returns(expectedTask);
+            //Act
+            var response = await _taskGrpcService.CreateRepairManagement(request, null);
+
+            // Assert
+            Assert.NotNull(response);
+            
+            Assert.Equal(expectedTask, response.IdTask);
+
+            // Kiểm tra tương tác
+            A.CallTo(() => _mockTaskService.CreateNewStaffTask(request)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task ListRepairManagement_ReturnOk()
+        {
+            //Arrange
+            var request = new GetListRepairManagementRequest
+            {
+                IdStaff = 1
+            };
+
+            var expectedTask = new List<ItemInListStaffTaskDto>
+            {
+                new ItemInListStaffTaskDto
+                {                    
+                    CustomerName = "Nguyễn Văn A",
+                    CustomerPhone = "098xx890xx",
+                    IdTask = 1,
+                    DateOfTask = DateOnly.Parse("12/01/2025"),
+                    DateOfWarranty = DateOnly.Parse("12/01/2024"),
+                    IdWarrantyRecord = 1,
+                    StatusTask = 1
+                }
+            };
+
+            // Cấu hình mock cho ICustomerService
+            A.CallTo(() => _mockTaskService.GetListStaffTask(request.IdStaff)).Returns(expectedTask);
+            //Act
+            var response = await _taskGrpcService.ListRepairManagement(request, null);
+
+            // Assert
+            Assert.NotNull(response);
+
+            Assert.Equal(expectedTask.Count, response.ToList.Count);
+
+            var firstCustomer = response.ToList.First();
+            Assert.Equal(expectedTask.First().IdWarrantyRecord, firstCustomer.IdWarrantRecord);
+            Assert.Equal(expectedTask.First().IdTask, firstCustomer.IdTask);
+            Assert.Equal(expectedTask.First().StatusTask, firstCustomer.StatusTask);
+            Assert.Equal("12/1/2024", firstCustomer.DateOfWarranty);
+            Assert.Equal(expectedTask.First().CustomerName, firstCustomer.CustomerName);
+            Assert.Equal(expectedTask.First().CustomerPhone, firstCustomer.CustomerPhone);
+            Assert.Equal("12/1/2025", firstCustomer.DateOfTask);
+
+            // Kiểm tra tương tác
+            A.CallTo(() => _mockTaskService.GetListStaffTask(request.IdStaff)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async Task GetStaffTask_ReturnOk()
+        {
+            //Arrange
+            var request = new ReadToRequest
+            {
+                IdTask = 1
+            };
+
+            var expectedTask = new List<ItemInListStaffTaskDto>
+            {
+                new ItemInListStaffTaskDto
+                {
+                    CustomerName = "Nguyễn Văn A",
+                    CustomerPhone = "098xx890xx",
+                    IdTask = 1,
+                    DateOfTask = DateOnly.Parse("12/01/2025"),
+                    DateOfWarranty = DateOnly.Parse("12/01/2024"),
+                    IdWarrantyRecord = 1,
+                    StatusTask = 1
+                }
+            };
+
+            // Cấu hình mock cho ICustomerService
+            A.CallTo(() => _mockTaskService.GetListStaffTask(request.IdStaff)).Returns(expectedTask);
+            //Act
+            var response = await _taskGrpcService.ListRepairManagement(request, null);
+
+            // Assert
+            Assert.NotNull(response);
+
+            Assert.Equal(expectedTask.Count, response.ToList.Count);
+
+            var firstCustomer = response.ToList.First();
+            Assert.Equal(expectedTask.First().IdWarrantyRecord, firstCustomer.IdWarrantRecord);
+            Assert.Equal(expectedTask.First().IdTask, firstCustomer.IdTask);
+            Assert.Equal(expectedTask.First().StatusTask, firstCustomer.StatusTask);
+            Assert.Equal("12/1/2024", firstCustomer.DateOfWarranty);
+            Assert.Equal(expectedTask.First().CustomerName, firstCustomer.CustomerName);
+            Assert.Equal(expectedTask.First().CustomerPhone, firstCustomer.CustomerPhone);
+            Assert.Equal("12/1/2025", firstCustomer.DateOfTask);
+
+            // Kiểm tra tương tác
+            A.CallTo(() => _mockTaskService.GetListStaffTask(request.IdStaff)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
